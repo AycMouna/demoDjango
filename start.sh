aee250ee627f1499996c2c4aaa0db1a24fec4a2a
Mouna <ayc.mouna@gmail.com>
2025-11-26 11:19:59 +0100

    Fix PORT configuration and enable DEBUG to diagnose issues

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 # Run database migrations
 python manage.py migrate --noinput
 
-# Start gunicorn server
-gunicorn --bind 0.0.0.0:8000 demoDjango.wsgi:application
+# Start gunicorn server on the PORT provided by Render
+gunicorn --bind 0.0.0.0:${PORT:-8000} demoDjango.wsgi:application
